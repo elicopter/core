f84300ac84c39d6af6aaf1d68682d70859079593
Loic Vigneron <loic.vigneron@gmail.com>
2017-03-02 20:11:53 +0100

    Update nerves...

--- config/prod.exs
+++ config/prod.exs
@@ -3,8 +3,11 @@ use Mix.Config
 config :core, :i2c, I2c
 config :core, :i2c_device_name, "i2c-1"
 config :core, :motor_pwm, Driver.PCA9685
-config :core, :receiver, Receiver.Ibus
-config :core, :uart, Nerves.UART
+# config :core, :receiver, Receiver.Ibus
+config :core, :receiver, Dummy.Ibus
+config :core, :uart, Dummy.UART
+
+# config :core, :uart, Nerves.UART
 config :core, :wifi, Nerves.InterimWiFi
 config :core, :black_box_rabbitmq,
   url: "amqp://core:core@192.168.142.100",
